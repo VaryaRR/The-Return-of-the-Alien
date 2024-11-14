5d8538d86085b53be07c59e599a150db04e94a78
VaryaRR <141347643+VaryaRR@users.noreply.github.com>
2024-11-14 18:50:36 +0300

    Add files via upload

---
+++ HelloWordl.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWordl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Проверка-тест
+        print("Hello, world!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
